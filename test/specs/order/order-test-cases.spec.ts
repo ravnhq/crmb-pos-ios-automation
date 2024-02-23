9aa9b7ac8f2d34e5cdc7e6afe57d2ba9c23156bf
Ruben Cuevas <rubencuevas@ravn.co>
2024-02-23 11:36:11 -0600

    Added test for CreateOrder_DriveTrhu_POS

--- test/specs/order/order-test-cases.spec.ts
+++ test/specs/order/order-test-cases.spec.ts
@@ -119,6 +119,24 @@ describe('CRMB-POS - Order Test Scenarios', () => {
         await checkoutScreen.tapOnDoneButton(testid);
     });
 
+    it('POS-007 Create Order / Drive Thru / Cash Payment', async () => {
+        await homeScreen.tapOnSearchBarButton(testid);
+        await homeScreen.typeOnSearchBarInput(testid, constants.testScriptConstants.searchItem);
+        await homeScreen.tapOnItemFirstResult(testid);
+        await menuItemDetailsScreen.tapOnAddToOrder(testid);
+        await homeScreen.tapOnCheckoutButton(testid);
+        await homeScreen.tapOnDriveThruText(testid);
+        await checkoutScreen.tapOnChargeCustomerButton(testid);
+        await checkoutScreen.tapOnContinueButton(testid);
+        await checkoutScreen.tapOnPayWithCashButton(testid);
+        await checkoutScreen.tapOnCalculateChangeButton(testid);
+        await checkoutScreen.typeInCodeNumber(testid, constants.testScriptConstants.employeeCode);
+        await checkoutScreen.tapOnContinueButton(testid);
+        await checkoutScreen.typeInCodeNumber(testid, constants.testScriptConstants.orderTotal);
+        await checkoutScreen.tapOnCompleteTransactionButton(testid);
+        await checkoutScreen.isOrderPlacedMessagePresent(testid);
+        await checkoutScreen.tapOnDoneButton(testid);
+    });
   
     it('POS-010- Create Order / Curbside / CC payment', async () => {
         await homeScreen.tapOnSearchBarButton(testid);
